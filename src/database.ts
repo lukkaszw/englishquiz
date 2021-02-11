60f61a0f18612d9c37688dade655a4bac4181cb5
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2021-02-11 18:02:19 +0100

    Add mongodb test database

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://127.0.0.1:27020/englishquiz';
+
+
+const url = process.env.NODE_ENV === 'production' ? 
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.r6yso.mongodb.net/englishquiz?retryWrites=true&w=majority` 
+  : 'mongodb://127.0.0.1:27020/englishquiz';
 
 mongoose.connect(url, {
   useNewUrlParser: true, 
